71ea76bc062ebe26db10124932c9425a715dc473
vasil <vaskonikolov2003@gmail.com>
2022-10-15 16:07:54 +0300

    doesnt work:

--- filter.py
+++ filter.py
@@ -6,24 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def compute_edges(image: np.ndarray, lower_bound=15, upper_bound=26) -> np.ndarray:
-    return image
-    # kernel_size_1 = max(5, 2 * int(0.0002 * image.shape[0]) + 1)
-    # kernel_size_2 = max(5, 2 * int(0.01 * image.shape[0]) + 1)
-    # blurred_image = cv2.GaussianBlur(image, (kernel_size_1, kernel_size_1), 0)
-    # plt.imshow(blurred_image)
-    # plt.show()
-    # return cv2.GaussianBlur(
-    #     cv2.Canny(
-    #         blurred_image,
-    #         lower_bound,
-    #         upper_bound,
-    #     ),
-    #     (kernel_size_2, kernel_size_2),
-    #     0,
-    # )
-
-
 def gradient_of_image(image):
     ksize = 5
     gX = cv2.Sobel(image, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=ksize)
@@ -43,21 +25,19 @@ def explore_edges(image):
     plt.show()
 
 
-def filter_image(image: np.ndarray):
+def filter_image(image: np.ndarray, random_coeff=0.0):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # blur image
+    blur_size = (2 * int(0.005 * min(image.shape)) + 1,) * 2
+    image = cv2.GaussianBlur(image, blur_size, 0)
     input_shape = image.shape
-
-    k = 15
-    edges = compute_edges(image, k, 2 * k)
-    # plt.imshow(edges)
-    # plt.show()
 
     scale_factor = 5000 / image.shape[0]
     output_shape = (np.array(image.shape) * scale_factor).astype(np.int16)
     out_image = np.zeros(output_shape)
-    N_lines = 50000
+    N_lines = 5000
     points_to_sample = []
-    edge_max = np.max(edges)
+    image_max = np.max(image)
     while len(points_to_sample) < N_lines:
         point = np.array(
             [
@@ -65,29 +45,36 @@ def filter_image(image: np.ndarray):
                 random.randint(0, input_shape[1] - 1),
             ]
         )
-        magnitude = edges[tuple(point)]
-        if magnitude > 0.3 * edge_max:
+        if image[tuple(point)] > 0.1 * image_max:
             points_to_sample.append(point)
 
-    gradient = gradient_of_image(image)
-    max_grad = np.max((gradient[:, :, 0] ** 2 + gradient[:, :, 1] ** 2)) ** 0.5
+    gradient = gradient_of_image(image).astype(np.float32)
+    gradient /= np.quantile(gradient, 0.99) / 255.0
+    gradient = np.clip(gradient, 0, 255.0)
+    # return (gradient[:, :, 0] ** 2 + gradient[:, :, 1] ** 2) ** 0.5
 
-    line_length = 0.1 * input_shape[0]
-    for i, p in enumerate(points_to_sample):
-        # if i % (N_lines // 20) == 0:
-        #     print("progress: ", i)
+    # max_grad = (
+    #     np.quantile((gradient[:, :, 0] ** 2 + gradient[:, :, 1] ** 2), 0.9) ** 0.5
+    # )
+
+    line_length = 0.2 * np.min(image.shape)
+    for p in points_to_sample:
         n = gradient[tuple(p)].astype(np.float32)
         n_len = (n[0] ** 2 + n[1] ** 2) ** 0.5
-        if n_len < 0.000001:
+        if n_len < 0.0001:
             continue
-        n_perpendicular = np.array([-n[1], n[0]]) / (max_grad**0.7 * n_len**0.3)
-        n_perpendicular = (
-            n_perpendicular + (np.random.random((2,)) - np.array([0.5, 0.5])) / 2000
-        )
+        n_perpendicular = np.array([-n[1], n[0]]) / n_len
+        # add randomness to the angle of the tangent line
+        # if random_coeff > 0.001:
+        #     n_perpendicular = (
+        #         n_perpendicular
+        #         + (np.random.random((2,)) - np.array([0.5, 0.5])) * random_coeff
+        #     )
 
-        strength = (image[tuple(p)] / 255) ** 1
+        strength = image[tuple(p)] / 255.0
+        # strength = min(n_len / max_grad, 1.0)
         color = np.array([255, 255, 255]) * strength
-        thichness = 1
+        thickness = 1
         start = np.flip(
             scale_factor * (p - (n_perpendicular * line_length)), axis=0
         ).astype(np.int16)
@@ -95,14 +82,13 @@ def filter_image(image: np.ndarray):
         end = np.flip(
             scale_factor * (p + (n_perpendicular * line_length)), axis=0
         ).astype(np.int16)
+
         cv2.line(
             out_image,
             start,
             end,
-            tuple(color),
-            thichness,
+            int(color[0]),
+            thickness,
         )
 
     return out_image
-    # plt.imshow(out_image, cmap="gray")
-    # plt.show()
--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from tqdm import tqdm
 
 import filter
 
-vidcap = cv2.VideoCapture("vid.mp4")
+vidcap = cv2.VideoCapture("vid_2.webm")
 success = True
 
 frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = vidcap.get(cv2.CAP_PROP_FPS)
 width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 out = cv2.VideoWriter(
-    "filename.avi", cv2.VideoWriter_fourcc(*"MJPG"), 30, (width, height)
+    "filename_2.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (width, height)
 )
 print(width, height)
 for i in tqdm(range(frame_count)):
     success, image = vidcap.read()
 
+    # if i > 10:
+    #     break
     if not success:
         print(f"error on frame {i}")
     filtered_im = cv2.resize(filter.filter_image(image), (width, height)).astype(
