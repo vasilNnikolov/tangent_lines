c741092ce8ce8ab378fdeb8e71d5b028a30aa7a3
vasil <vaskonikolov2003@gmail.com>
2022-10-15 19:59:45 +0300

    video works decently well

--- filter.py
+++ filter.py
@@ -1,6 +1,3 @@
-import random
-import sys
-
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,57 +10,45 @@ def gradient_of_image(image):
     return np.stack([gY, gX], axis=2)
 
 
-def explore_edges(image):
-    n = 5
-    fig, ax = plt.subplots(n, n)
-    for i in range(n):
-        lower_bound = 10 + 4 * i
-        for j in range(n):
-            upper_bound = lower_bound + j * 4
-            ax[i, j].imshow(compute_edges(image, lower_bound, upper_bound))
-            ax[i, j].title.set_text(f"Lower {lower_bound}, upper {upper_bound}")
-    plt.show()
-
-
-def filter_image(image: np.ndarray, random_coeff=0.0):
+def filter_image(image: np.ndarray, scale_factor=4) -> np.ndarray:
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # blur image
-    blur_size = (2 * int(0.005 * min(image.shape)) + 1,) * 2
+    blur_size = (2 * int(0.01 * min(image.shape)) + 1,) * 2
     image = cv2.GaussianBlur(image, blur_size, 0)
-    input_shape = image.shape
 
-    scale_factor = 5000 / image.shape[0]
     output_shape = (np.array(image.shape) * scale_factor).astype(np.int16)
-    out_image = np.zeros(output_shape)
-    N_lines = 50000
-    points_to_sample = []
-    image_max = np.max(image)
-    while len(points_to_sample) < N_lines:
-        point = np.array(
-            [
-                random.randint(0, input_shape[0] - 1),
-                random.randint(0, input_shape[1] - 1),
-            ]
-        )
-        if image[tuple(point)] > 0.1 * image_max:
-            points_to_sample.append(point)
+    out_image = np.zeros(tuple(output_shape)).astype(np.uint8)
 
     gradient = gradient_of_image(image).astype(np.float32)
     gradient_magnitude = (gradient[:, :, 0] ** 2 + gradient[:, :, 1] ** 2) ** 0.5
-    gradient_magnitude /= np.quantile(gradient_magnitude, 0.99) / 255.0
+
+    # scale gradient in range (0, 255)
+    gradient_scaling_coefficient = np.quantile(gradient_magnitude, 0.999) / 200.0
+    gradient_magnitude /= gradient_scaling_coefficient
     gradient_magnitude = np.clip(gradient_magnitude, 0, 255)
+
+    gradient /= gradient_scaling_coefficient
+    # plt.hist(gradient_magnitude.flatten(), bins=100)
+    # plt.show()
+
+    edge_coords = np.moveaxis(np.mgrid[0 : image.shape[0], 0 : image.shape[1]], 0, -1)[
+        gradient_magnitude > np.quantile(gradient_magnitude, 0.8)
+    ]
+    N = min(len(edge_coords), 10000)
+    edge_coords = edge_coords[np.random.randint(0, len(edge_coords), N)]
 
     # return gradient_magnitude
 
-    line_length = 0.1 * np.min(image.shape)
-    for p in points_to_sample:
+    line_length = 0.03 * np.min(image.shape)
+    for p in edge_coords:
         n = gradient[tuple(p)].astype(np.float32)
         n_len = gradient_magnitude[tuple(p)]
         if n_len < 0.0001:
             continue
         n_perpendicular = np.array([-n[1], n[0]]) / n_len
 
-        color = int(n_len)
+        color = int(((n_len / 255) ** 0.5) * 255)
+        # color = int(image[tuple(p)])
         thickness = 1
         start = np.flip(
             scale_factor * (p - (n_perpendicular * line_length)), axis=0
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 from tqdm import tqdm
 
 import filter
@@ -9,20 +8,26 @@ success = True
 
 frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 fps = vidcap.get(cv2.CAP_PROP_FPS)
-width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-out = cv2.VideoWriter(
-    "filename_2.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (width, height)
+input_width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
+input_height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+scaling_coefficient = 2
+output_width, output_height = (
+    int(scaling_coefficient * input_width),
+    int(scaling_coefficient * input_height),
 )
-print(width, height)
-for i in tqdm(range(10)):
-    # for i in tqdm(range(frame_count)):
+out = cv2.VideoWriter(
+    "filename_2_blur_10px.avi",
+    cv2.VideoWriter_fourcc(*"MJPG"),
+    fps,
+    (output_width, output_height),
+)
+# for i in tqdm(range(1)):
+for i in tqdm(range(frame_count)):
     success, image = vidcap.read()
     if not success:
         print(f"error on frame {i}")
-    filtered_im = cv2.resize(filter.filter_image(image), (width, height)).astype(
-        np.uint8
-    )
+    filtered_im = filter.filter_image(image, scale_factor=scaling_coefficient)
     transformed_image = cv2.cvtColor(filtered_im, cv2.COLOR_GRAY2BGR)
     out.write(transformed_image)
 
