db94fb63565eb1985ca10df61e75097e8bc94ab8
vasil <vaskonikolov2003@gmail.com>
2022-10-15 21:55:17 +0300

    minor comments

--- filter.py
+++ filter.py
@@ -5,11 +5,13 @@ import numpy as np
 
 def gradient_of_image(image):
     image = image.astype(np.float32)
-    ksize = 5
-    gX = cv2.Sobel(image, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=ksize)
-    gY = cv2.Sobel(image, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=ksize)
+    ksize = 7
+    # gX = cv2.Scharr(image, ddepth=cv2.CV_32F, dx=1, dy=0).astype(np.float32)
+    # gY = cv2.Scharr(image, ddepth=cv2.CV_32F, dx=0, dy=1).astype(np.float32)
+    gX = cv2.Sobel(image, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=ksize).astype(np.float32)
+    gY = cv2.Sobel(image, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=ksize).astype(np.float32)
     # tries to kind of normalise the gradient, since brighter areas have higher gradient
-    offset = 1
+    offset = 10
     return np.stack([gY / (image + offset), gX / (image + offset)], axis=2)
 
 
@@ -20,15 +22,13 @@ def filter_image(image: np.ndarray, scale_factor=4) -> np.ndarray:
     blur_size = (2 * int(0.007 * min(image.shape)) + 1,) * 2
     image = cv2.GaussianBlur(image, blur_size, 0)
 
-    output_shape = (np.array(image.shape) * scale_factor).astype(np.int16)
-    out_image = np.zeros(tuple(output_shape)).astype(np.uint8)
-
-    gradient = gradient_of_image(image).astype(np.float32)
+    gradient = gradient_of_image(image)
     gradient_magnitude = (gradient[:, :, 0] ** 2 + gradient[:, :, 1] ** 2) ** 0.5
 
     edge_coords = np.moveaxis(np.mgrid[0 : image.shape[0], 0 : image.shape[1]], 0, -1)[
         gradient_magnitude > np.quantile(gradient_magnitude, 0.8)
     ]
+    # the number of lines that will be drawn
     N = min(len(edge_coords), 10000)
     edge_coords = edge_coords[np.random.randint(0, len(edge_coords), N)]
 
@@ -53,6 +53,9 @@ def filter_image(image: np.ndarray, scale_factor=4) -> np.ndarray:
         ],
         axis=1,
     )
+
+    output_shape = (np.array(image.shape) * scale_factor).astype(np.int16)
+    out_image = np.zeros(tuple(output_shape)).astype(np.uint8)
 
     # drawing parameters
     line_length = 0.03 * np.min(image.shape)
